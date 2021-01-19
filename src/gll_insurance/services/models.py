48e40c669220c6915a0295f87d6ee9fd2b67ddf0
Daniel Gallagher <daniel.gallagher1@ucdconnect.ie>
2021-01-19 10:50:15 +0000

    basic service impl

--- src/gll_insurance/services/models.py
+++ src/gll_insurance/services/models.py
@@ -7,3 +7,10 @@ class Service(models.Model):
 
 	def __str__(self):
 		return self.title
+
+class ServiceList(models.Model):
+	title = models.CharField(max_length=100)
+	queryset = Service.objects.all()
+
+	def __str__(self):
+		return self.title
