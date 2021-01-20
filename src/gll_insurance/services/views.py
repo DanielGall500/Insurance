e13aa19c21a4a2d2b4db041864a7094216df9a31
Daniel Gallagher <daniel.gallagher1@ucdconnect.ie>
2021-01-20 19:30:41 +0000

    basic navigation bar

--- src/gll_insurance/services/views.py
+++ src/gll_insurance/services/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.views.generic.detail import ListView
+from django.views.generic import ListView
 from .models import Service
 
 class ServiceView(ListView):
 	model = Service
+	template_name = 'services/index.html'
+
+	def get_queryset(self):
+		return Service.objects.all()
