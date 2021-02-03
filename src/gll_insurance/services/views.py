4f90197ed08f72f2f8a246fdf8b8939310ba87ea
Daniel Gallagher <daniel.gallagher1@ucdconnect.ie>
2021-02-03 22:21:24 +0000

    card deck added

--- src/gll_insurance/services/views.py
+++ src/gll_insurance/services/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
+from django.views import generic
 from .models import Service, ServiceList
 
-class ServiceView(ListView):
+class ServiceView(generic.ListView):
 	model = Service
 	template_name = 'services/index.html'
 
+	context_object_name = 'services_list'
 	def get_queryset(self):
 		return Service.objects.all()
-
-def index(request):
-	services_list = Service.objects.all()
-	context = { 'services_list' : services_list }
-	return render(request, 'services/index.html', context)
 
 def description(request, pk):
 	service = get_object_or_404(Service, pk=pk)
